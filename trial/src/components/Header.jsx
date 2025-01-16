cd3beead03f57b34425c37f5203a0aa856a5f135
Ishaan Jain <ishaanj2612@gmail.com>
2025-01-16 06:02:28 +0530

    COMPLETE Header Search Form Functionality

--- trial/src/components/Header.jsx
+++ trial/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { assets } from '../assets/assets'
 import { AppContent } from '../context/AppContext';
 import axios from 'axios'
@@ -9,6 +9,7 @@ import { toast } from 'react-toastify';
 export default function Header() {
   const { backendURL, userData, setUserListings, getUserListings, setUserData, setIsLoggedIn } = useContext(AppContent)
   const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
 
   const sendVerificationOtp = async () => {
     try {
@@ -60,7 +61,18 @@ export default function Header() {
       console.log(e)
     }
   }
-
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  }
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if (searchTermFromUrl) setSearchTerm(searchTermFromUrl)
+  }, [])
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -72,12 +84,14 @@ export default function Header() {
           </h1>
         </Link>
         <form
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
           className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
             <FaSearch className='text-slate-600' />
